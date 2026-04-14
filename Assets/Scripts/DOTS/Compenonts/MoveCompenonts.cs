2a7bb4ae3c4c8e20f1ff97d95738036b7ad99c4c
admin <812094204@qq.com>
2026-04-15 01:03:49 +0800

    初步进行DOTS改造

---
+++ Assets/Scripts/DOTS/Compenonts/MoveCompenonts.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics; // 使用 Unity.Mathematics 替代 Mathf 和 Vector3
+
+public struct Position : IComponentData {
+    public float3 Value;
+}
+
+public struct Velocity : IComponentData {
+    public float2 Value;
+}
